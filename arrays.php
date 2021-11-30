946e41a9da16471fc813a3dfda6c7e4038a481a0
MHU-Helal <info.m.helal@gmail.com>
2021-12-01 05:14:25 +0600

    function and array php added and included in the index file.

---
+++ arrays.php
@@ -0,0 +1,65 @@
+<?php
+    // Food array
+    $food   =   ['Potato','Carrot','Cabbage','Radish','Chilli','Tomato'];
+
+    // Location Array
+    $location   =   [
+        'Mirpur',
+        'Azimpur',
+        'Azompur',
+        'Dhanmondi',
+        'Mohakhali',
+        'Gazipur'
+    ];
+
+    // Name Array
+    $name   =   [
+        'Asraful Hoque',
+        'Nahiyan Khandokar',
+        'Mohsina Akter',
+        'Ridu Ahmed',
+        'Imdadul Hoque',
+        'Shahnewaj',
+        'Sabbir Khan'
+    ];
+
+    // Customer Array
+    $customers  =   [
+        'Asraful Hoque' =>  [
+            'name'  =>   'Asraful Hoque',
+            'job'   =>  'Website Developer',
+            'cell'  =>  '01710000000'
+        ],
+
+        'Mohsina Akter' =>  [
+            'name'  =>   'Mohsina Akter',
+            'job'   =>  'Student',
+            'cell'  =>  '01711111111'
+        ],
+
+        'Nahiyan Khandokar' =>  [
+            'name'  =>   'Nahiyan Khondokar',
+            'job'   =>  'Student',
+            'cell'  =>  '01712222222'
+        ],
+
+        'Ridu Ahmed' =>  [
+            'name'  =>   'Ridu Ahmed',
+            'job'   =>  'Student',
+            'cell'  =>  '01713333333'
+        ],
+
+        'Imdadul Hoque' =>  [
+            'name'  =>   'Imdadul Hoque',
+            'job'   =>  'Student',
+            'cell'  =>  '01714444444'
+        ],
+
+        'Shahnewaj' =>  [
+            'name'  =>   'Shahnewaj',
+            'job'   =>  'Student',
+            'cell'  =>  '01715555555'
+        ],
+
+        
+    ];
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Include once all required php files in here!
+ */
+include_once "functions.php";
+include_once "arrays.php";
+
+
+
+
+
+?>
